c02e746a0239a856413b8cfe860eb4729049b3e9
David Perez <dpereze1105@hotmail.com>
2022-11-24 22:49:56 -0600

    Ktlint added

--- app/src/main/java/com/flknlabs/gitlabci_example/MainActivity.kt
+++ app/src/main/java/com/flknlabs/gitlabci_example/MainActivity.kt
@@ -1,8 +1,8 @@
 package com.flknlabs.gitlabci_example
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
